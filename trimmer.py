5391f57a684872794355fe8c5720656e44b54bda
Abhimanyu Saharan <asaharan@onemindservices.com>
2025-01-14 05:13:32 +0530

    Update trimmer.py
    Signed-off-by: Abhimanyu Saharan <asaharan@onemindservices.com>

--- trimmer.py
+++ trimmer.py
@@ -11,7 +11,7 @@ with open("bitnami/index.yaml", "r") as f:
 print("Read the entries...")
 # Remove all the entries except postgresql-ha
 for key in list(data["entries"].keys()):
-    if key != "postgresql-ha":
+    if key != "kube-state-metrics":
         print("Removing entry: %s" % key)
         del data["entries"][key]
 
